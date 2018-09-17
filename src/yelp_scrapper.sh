b3956c797456f5b21c87296b72db0c17d7d3bd19
Steve Griswold <steve.griswold@usfoods.com>
2018-09-17 07:36:05 -0700

    yelp scrapper class for iterating around locations.

--- src/yelp_scrapper.sh
+++ src/yelp_scrapper.sh
@@ -1,5 +1,22 @@
 echo "After ssh dsi is running"
 echo "script copies files to use"
 
-scp ../.gitignore/yelp_api_key.yaml dsi:yelp_api_key.yaml
-scp ../data/yelp_businesses.pickle dsi:yelp_businesses.pickle
+scp ../.gitignore/yelp_api_key.yaml macdevro:yelp_api_key.yaml
+scp ../data/yelp_businesses.pickle macdevro:yelp_businesses.pickle
+
+scp server_setup.sh macdevro:src/server_setup.sh
+
+echo "done moving files"
+
+echo "go to the server and run sh server_setup.sh"
+echo -n "Press Enter when ssh server is ready!"
+read answer
+
+echo "Thanks"
+
+echo "Now moving flask app files"
+scp -r dashboard/static/ macdevro:flaskapp/static/
+scp -r dashboard/templates/ macdevro:flaskapp/templates/
+
+echo "Now moving data"
+scp -r data/ macdevro:
